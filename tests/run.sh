e594832a89611aaa2a98d305d48499b7aaddc48e
Milan Pultar <milan.pultar@gmail.com>
2024-10-14 01:01:10 +0200

    fixing tests

--- tests/run.sh
+++ tests/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+alias DOCKER_RUN='docker run --rm -v $GITHUB_WORKSPACE/output:/data/output -v $(pwd)/tests:/data/tests -v $GITHUB_WORKSPACE/samples:/data/protos protocpolyglot/protoc-polyglot'
+
 pushd /core
 
-for folder in /core/*; do
+for folder in /data/protoc_polyglot/*; do
     echo $folder
     folder=${folder%/}  # Remove trailing slash
     if [ -d "$folder" ] && [ "$(basename "$folder")" != "__pycache__" ]; then
         echo "Processing folder: $folder"
-        cli.py --languages $folder --service-yml /data/protos/services.yml
+        DOCKER_RUN --languages $folder --service-yml /data/protos/services.yml
     fi
 done
 
