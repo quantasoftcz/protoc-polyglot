cfca526b2675a9a50831e47b4a6d7ff9a23bca47
Milan Pultar <milan.pultar@gmail.com>
2024-10-14 11:16:14 +0200

    fixing tests

--- tests/run.sh
+++ tests/run.sh
@@ -2,7 +2,7 @@
 
 alias DOCKER_RUN="docker run --rm -v $GITHUB_WORKSPACE/output:/data/output -v $GITHUB_WORKSPACE/tests:/data/tests -v $GITHUB_WORKSPACE/samples:/data/protos protocpolyglot/protoc-polyglot"
 
-for folder in /data/protoc_polyglot/*; do
+for folder in $GITHUB_WORKSPACE/protoc_polyglot/*; do
     echo $folder
     folder=${folder%/}  # Remove trailing slash
     if [ -d "$folder" ] && [ "$(basename "$folder")" != "__pycache__" ]; then
